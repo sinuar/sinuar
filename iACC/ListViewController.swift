e9fe96bdd12a3a7c84e8e245d0ffac1f05add9ec
sinuar <sinuar2@gmail.com>
2022-09-22 14:01:25 -0500

    Create ItemViewModel in the right context to improve the design and eliminate fatal errors required by untyped models.

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -5,178 +5,181 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-	var items = [ItemViewModel]()
-	
-	var retryCount = 0
-	var maxRetryCount = 0
-	var shouldRetry = false
-	
-	var longDateStyle = false
-	
-	var fromReceivedTransfersScreen = false
-	var fromSentTransfersScreen = false
-	var fromCardsScreen = false
-	var fromFriendsScreen = false
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		refreshControl = UIRefreshControl()
-		refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
-		
-		if fromFriendsScreen {
-			shouldRetry = true
-			maxRetryCount = 2
-			
-			title = "Friends"
-			
-			navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
-			
-		} else if fromCardsScreen {
-			shouldRetry = false
-			
-			title = "Cards"
-			
-			navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addCard))
-			
-		} else if fromSentTransfersScreen {
-			shouldRetry = true
-			maxRetryCount = 1
-			longDateStyle = true
-
-			navigationItem.title = "Sent"
-			navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(sendMoney))
-
-		} else if fromReceivedTransfersScreen {
-			shouldRetry = true
-			maxRetryCount = 1
-			longDateStyle = false
-			
-			navigationItem.title = "Received"
-			navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Request", style: .done, target: self, action: #selector(requestMoney))
-		}
-	}
-	
-	override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-		
-		if tableView.numberOfRows(inSection: 0) == 0 {
-			refresh()
-		}
-	}
-	
-	@objc private func refresh() {
-		refreshControl?.beginRefreshing()
-		if fromFriendsScreen {
-			FriendsAPI.shared.loadFriends { [weak self] result in
-				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
-				}
-			}
-		} else if fromCardsScreen {
-			CardAPI.shared.loadCards { [weak self] result in
-				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
-				}
-			}
-		} else if fromSentTransfersScreen || fromReceivedTransfersScreen {
-			TransfersAPI.shared.loadTransfers { [weak self] result in
-				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
-				}
-			}
-		} else {
-			fatalError("unknown context")
-		}
-	}
-	
-	private func handleAPIResult<T>(_ result: Result<[T], Error>) {
-		switch result {
-		case let .success(items):
-			if fromFriendsScreen && User.shared?.isPremium == true {
-				(UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(items as! [Friend])
-			}
-			self.retryCount = 0
-			
-			var filteredItems = items as [Any]
-			if let transfers = items as? [Transfer] {
-				if fromSentTransfersScreen {
-					filteredItems = transfers.filter(\.isSender)
-				} else {
-					filteredItems = transfers.filter { !$0.isSender }
-				}
-			}
-			
-            self.items = filteredItems.map { item in
-               ItemViewModel(item, longDateStyle: longDateStyle, selection: { [weak self] in
-                  if let friend = item as? Friend {
-                     self?.select(friend: friend)
-                  } else if let card = item as? Card {
-                     self?.select(card: card)
-                  } else if let transfer = item as? Transfer {
-                     self?.select(transfer: transfer)
-                  } else {
-                     fatalError("unknown item: \(item)")
+   var items = [ItemViewModel]()
+   
+   var retryCount = 0
+   var maxRetryCount = 0
+   var shouldRetry = false
+   
+   var longDateStyle = false
+   
+   var fromReceivedTransfersScreen = false
+   var fromSentTransfersScreen = false
+   var fromCardsScreen = false
+   var fromFriendsScreen = false
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      refreshControl = UIRefreshControl()
+      refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+      
+      if fromFriendsScreen {
+         shouldRetry = true
+         maxRetryCount = 2
+         
+         title = "Friends"
+         
+         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
+         
+      } else if fromCardsScreen {
+         shouldRetry = false
+         
+         title = "Cards"
+         
+         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addCard))
+         
+      } else if fromSentTransfersScreen {
+         shouldRetry = true
+         maxRetryCount = 1
+         longDateStyle = true
+         
+         navigationItem.title = "Sent"
+         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(sendMoney))
+         
+      } else if fromReceivedTransfersScreen {
+         shouldRetry = true
+         maxRetryCount = 1
+         longDateStyle = false
+         
+         navigationItem.title = "Received"
+         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Request", style: .done, target: self, action: #selector(requestMoney))
+      }
+   }
+   
+   override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      
+      if tableView.numberOfRows(inSection: 0) == 0 {
+         refresh()
+      }
+   }
+   
+   @objc private func refresh() {
+      refreshControl?.beginRefreshing()
+      if fromFriendsScreen {
+         FriendsAPI.shared.loadFriends { [weak self] result in
+            DispatchQueue.mainAsyncIfNeeded {
+               self?.handleAPIResult(result.map { items in
+                  if User.shared?.isPremium == true {
+                     (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(items)
+                  }
+                  
+                  return items.map { item in
+                     ItemViewModel(friend: item, selection: {
+                        self?.select(friend: item)
+                     })
                   }
                })
             }
-			self.refreshControl?.endRefreshing()
-			self.tableView.reloadData()
-			
-		case let .failure(error):
-			if shouldRetry && retryCount < maxRetryCount {
-				retryCount += 1
-				
-				refresh()
-				return
-			}
-			
-			retryCount = 0
-			
-			if fromFriendsScreen && User.shared?.isPremium == true {
-				(UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.loadFriends { [weak self] result in
-					DispatchQueue.mainAsyncIfNeeded {
-						switch result {
-						case let .success(items):
-                        self?.items = items.map { item in
-                           ItemViewModel(friend: item, selection: { [weak self] in
-                              self?.select(friend: item)
+         }
+      } else if fromCardsScreen {
+         CardAPI.shared.loadCards { [weak self] result in
+            DispatchQueue.mainAsyncIfNeeded {
+               self?.handleAPIResult(result.map { items in
+                  items.map { item in
+                     ItemViewModel(card: item, selection: {
+                        self?.select(card: item)
+                     })
+                  }
+               })
+            }
+         }
+      } else if fromSentTransfersScreen || fromReceivedTransfersScreen {
+         TransfersAPI.shared.loadTransfers { [weak self, longDateStyle, fromSentTransfersScreen] result in
+            DispatchQueue.mainAsyncIfNeeded {
+               self?.handleAPIResult(result.map { items in
+                  items
+                     .filter { fromSentTransfersScreen ? $0.isSender :
+                        !$0.isSender}
+                     .map { item in
+                        ItemViewModel(
+                           transfer: item,
+                           longDateStyle: longDateStyle, selection: {
+                              self?.select(transfer: item)
                            })
-                        }
-							self?.tableView.reloadData()
-							
-						case let .failure(error):
-                     self?.show(error: error)
-						}
-						self?.refreshControl?.endRefreshing()
-					}
-				}
-			} else {
-            self.show(error: error)
-				self.refreshControl?.endRefreshing()
-			}
-		}
-	}
-	
-	override func numberOfSections(in tableView: UITableView) -> Int {
-		1
-	}
-	
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		items.count
-	}
-	
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let item = items[indexPath.row]
-		let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "ItemCell")
+                     }
+               })
+            }
+         }
+      } else {
+         fatalError("unknown context")
+      }
+   }
+   
+   private func handleAPIResult(_ result: Result<[ItemViewModel], Error>) {
+      switch result {
+         case let .success(items):
+            
+            self.retryCount = 0
+            self.items = items
+            self.refreshControl?.endRefreshing()
+            self.tableView.reloadData()
+            
+         case let .failure(error):
+            if shouldRetry && retryCount < maxRetryCount {
+               retryCount += 1
+               
+               refresh()
+               return
+            }
+            
+            retryCount = 0
+            
+            if fromFriendsScreen && User.shared?.isPremium == true {
+               (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.loadFriends { [weak self] result in
+                  DispatchQueue.mainAsyncIfNeeded {
+                     switch result {
+                        case let .success(items):
+                           self?.items = items.map { item in
+                              ItemViewModel(friend: item, selection: { [weak self] in
+                                 self?.select(friend: item)
+                              })
+                           }
+                           self?.tableView.reloadData()
+                           
+                        case let .failure(error):
+                           self?.show(error: error)
+                     }
+                     self?.refreshControl?.endRefreshing()
+                  }
+               }
+            } else {
+               self.show(error: error)
+               self.refreshControl?.endRefreshing()
+            }
+      }
+   }
+   
+   override func numberOfSections(in tableView: UITableView) -> Int {
+      1
+   }
+   
+   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      items.count
+   }
+   
+   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let item = items[indexPath.row]
+      let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "ItemCell")
       cell.configure(item)
-		return cell
-	}
-	
-	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		let item = items[indexPath.row]
+      return cell
+   }
+   
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      let item = items[indexPath.row]
       item.select()
-	}
+   }
    
 }
 
@@ -197,7 +200,7 @@ struct ItemViewModel {
       }
    }
 }
-   
+
 extension ItemViewModel {
    init(friend: Friend, selection: @escaping () -> Void) {
       title = friend.name
@@ -278,7 +281,7 @@ extension UIViewController {
    @objc func requestMoney() {
       show(RequestMoneyViewController(), sender: self)
    }
-
+   
    func show(error: Error) {
       let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
       alert.addAction(UIAlertAction(title: "Ok", style: .default))
